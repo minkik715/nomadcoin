e0fbe795716fa74f3032ee8ec1c62be93dc8753a
rlaalsrl715 <minkik715@gmail.com>
2023-02-20 22:30:33 +0900

    feat: rest API

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "github.com/rlaalsrl715/nomadcoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URL string
+
+type URLDescription struct {
+	URL         URL    `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+	CreatedAt   int64  `json:"-"`
+}
+
+func (u URL) MarshalText() ([]byte, error) {
+	return []byte(fmt.Sprintf("http://localhost%s%s", port, u)), nil
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         URL("/"),
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL:         URL("/block"),
+			Method:      "POST",
+			Description: "Add Block",
+			Payload:     "data:string",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("LIstening on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
