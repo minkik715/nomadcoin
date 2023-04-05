ff326e22b53b6177f6a23586e99b7ef478a43365
rlaalsrl715 <minkik715@gmail.com>
2023-04-05 20:55:45 +0900

    feat: channel close

--- main.go
+++ main.go
@@ -5,24 +5,31 @@ import (
 	"time"
 )
 
-func countToTen(c chan int) {
+func countToTen(c chan<- int) {
 	for i := range [10]int{} {
-		time.Sleep(time.Second * 5)
+		time.Sleep(time.Second * 2)
 		fmt.Printf("sending -> %d\n", i)
 		c <- i
 	}
+	close(c)
 }
 
 func main() {
 	c := make(chan int)
 	go countToTen(c)
 
-	for {
-		a := <-c
-		fmt.Printf("recieve -> %d\n", a)
-		if c == nil {
-			println("GO ROUTINE FINISH")
-		}
-	}
+	receive(c)
 
+}
+
+func receive(c <-chan int) {
+	for {
+		a, ok := <-c
+		if ok != true {
+			println("GO ROUTINE FINISH")
+			break
+		}
+		fmt.Printf("recieve -> %d\n", a)
+
+	}
 }
