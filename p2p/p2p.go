ea286bc30e40968520fad44414967efcb2a231d1
rlaalsrl715 <minkik715@gmail.com>
2023-04-08 14:57:10 +0900

    feat: make connection

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,32 +1,35 @@
 package p2p
 
 import (
-	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/rlaalsrl715/nomadcoin/utils"
 	"net/http"
-	"time"
 )
 
 var upgrader = websocket.Upgrader{}
+
+var conns []*websocket.Conn
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	upgrader.CheckOrigin = func(r *http.Request) bool {
 		return true
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
+	conns = append(conns, conn)
 	utils.HandleErr(err)
 	for {
 		_, p, err := conn.ReadMessage()
 		if err != nil {
+
 			err = conn.Close()
 			utils.HandleErr(err)
 			break
 		}
-		fmt.Printf("Just got %s\n\n", p)
-		time.Sleep(time.Second * 5)
-		message := fmt.Sprintf("we also think thant %s", p)
-		err = conn.WriteMessage(websocket.TextMessage, []byte(message))
-		utils.HandleErr(err)
+		for _, aConn := range conns {
+			if aConn != conn {
+				err = aConn.WriteMessage(websocket.TextMessage, p)
+				utils.HandleErr(err)
+			}
+		}
 	}
 }
